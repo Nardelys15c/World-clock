73adcea40575e17afb21b8d4fc1e27eb683beb1c
Nardelys <nardelys15c@gmail.com>
2023-09-15 21:08:49 -0400

    added time and date elements on JS

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let veniceElement = document.querySelector("#venice");
+  let veniceDateElement = veniceElement.querySelector(".date");
+  let veniceTimeElement = veniceElement.querySelector(".time");
+  let veniceTime = moment().tz("Europe/Rome");
 
+  veniceDateElement.innerHTML = veniceTime.format("MMMM Do, YYYY");
+  veniceTimeElement.innerHTML = veniceTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let dallasElement = document.querySelector("#dallas");
+  let dallasDateElement = dallasElement.querySelector(".date");
+  let dallasTimeElement = dallasElement.querySelector(".time");
+  let dallasTime = moment().tz("America/Matamoros");
+
+  dallasDateElement.innerHTML = dallasTime.format("MMMM Do, YYYY");
+  dallasTimeElement.innerHTML = dallasTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
